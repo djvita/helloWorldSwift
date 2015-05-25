09412a67d0ed3faa1af45a317a134b9c77c118a3
Vita V <dj.vita.09@gmail.com>
2015-05-25 13:46:03 -0500

    Added Outlets and Actions to elements

--- helloWorldSwift/ViewController.swift
+++ helloWorldSwift/ViewController.swift
@@ -4,12 +4,20 @@
 //
 //  Created by vita on 5/25/15.
 //  Copyright (c) 2015 vita. All rights reserved.
-//
+//  sup!
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    @IBAction func goTapped(sender: AnyObject) {
+        helloLabel.text = "Hello World!"
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
