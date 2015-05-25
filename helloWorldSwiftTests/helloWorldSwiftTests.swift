39b0568d86542469828cf9dcfc7837281277be05
Vita V <dj.vita.09@gmail.com>
2015-05-25 12:56:32 -0500

    Initial Commit

---
+++ helloWorldSwiftTests/helloWorldSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  helloWorldSwiftTests.swift
+//  helloWorldSwiftTests
+//
+//  Created by vita on 5/25/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class helloWorldSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
